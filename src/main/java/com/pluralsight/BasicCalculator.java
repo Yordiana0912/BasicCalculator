0328572f39430ff4cff1eecb7849f639efa2224c
Yordiana0912 <Yordiana0912@gmail.com>
2024-04-12 11:51:42 -0400

    Completed Mob3- Excercise 3- Basic Calculator

---
+++ src/main/java/com/pluralsight/BasicCalculator.java
@@ -0,0 +1,24 @@
+package com.pluralsight;
+
+import java.util.*;
+public class BasicCalculator {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print(12);
+
+        int num1 = myScanner.nextInt();
+        System.out.print(5);
+
+        int num2 = myScanner.nextInt();
+        int sum= num1 + num2;
+
+        System.out.println("What is sum");
+        int s = myScanner.nextInt();
+
+
+
+
+
+
+    }
+}
